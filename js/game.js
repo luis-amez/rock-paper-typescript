cb4ef2ffcafa238d27378caf20e6ffeb013bd799
Luis Amez Vitoria <luisamez@google.com>
2019-08-19 12:30:40 +0100

    Add basic functionality to start the game

--- js/game.js
+++ js/game.js
@@ -1,5 +1,18 @@
-alert("Hello world! I love Typescript!");
 var userScore = 0;
 var computerScore = 0;
 var counter = 0;
+function startGame() {
+    userScore = 0;
+    computerScore = 0;
+    counter = 0;
+    outputMessage("The game has begun!");
+    hideStartButton();
+}
+function outputMessage(message) {
+    alert(message);
+}
+function hideStartButton() {
+    var startButton = document.querySelector(".start-button");
+    startButton.style.display = "none";
+}
 //# sourceMappingURL=game.js.map
--- ts/game.ts
+++ ts/game.ts
@@ -1,4 +1,20 @@
-alert("Hello world! I love Typescript!");
 let userScore: number = 0;
 let computerScore: number = 0;
 let counter: number = 0;
+
+function startGame(): void {
+    userScore = 0;
+    computerScore = 0;
+    counter = 0;
+    outputMessage("The game has begun!");
+    hideStartButton();
+}
+
+function outputMessage(message: string): void {
+    alert(message);
+}
+
+function hideStartButton(): void {
+    let startButton: HTMLButtonElement = document.querySelector(".start-button");
+    startButton.style.display = "none";
+}
