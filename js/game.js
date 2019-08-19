2d6a68211b52513562cea9dd6ee70a63f4bc4d38
Luis Amez Vitoria <luisamez@google.com>
2019-08-19 12:16:48 +0100

    Setup Typescript

---
+++ js/game.js
@@ -0,0 +1,5 @@
+alert("Hello world! I love Typescript!");
+var userScore = 0;
+var computerScore = 0;
+var counter = 0;
+//# sourceMappingURL=game.js.map
---
+++ ts/game.ts
@@ -0,0 +1,4 @@
+alert("Hello world! I love Typescript!");
+let userScore: number = 0;
+let computerScore: number = 0;
+let counter: number = 0;
