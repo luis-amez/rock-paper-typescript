fc5a6dc5087a143bf5461e0ee33460e3010884b9
Luis Amez Vitoria <luisamez@google.com>
2019-08-19 14:56:30 +0100

    Go crazy and implement the rest of game's logic

--- js/game.js
+++ js/game.js
@@ -1,3 +1,9 @@
+var moves;
+(function (moves) {
+    moves[moves["Rock"] = 0] = "Rock";
+    moves[moves["Paper"] = 1] = "Paper";
+    moves[moves["Scissors"] = 2] = "Scissors";
+})(moves || (moves = {}));
 var userScore = 0;
 var computerScore = 0;
 var counter = 0;
@@ -9,10 +15,62 @@ function startGame() {
     hideStartButton();
 }
 function outputMessage(message) {
-    alert(message);
+    var output = document.querySelector(".output");
+    output.innerHTML += "<p>" + message + "</p>";
+}
+function clearMessage() {
+    var output = document.querySelector(".output");
+    output.innerHTML = "";
 }
 function hideStartButton() {
     var startButton = document.querySelector(".start-button");
+    var gameButtons = document.querySelector(".game-buttons");
     startButton.style.display = "none";
+    gameButtons.style.display = "block";
+}
+function getComputerMove() {
+    var move = Math.floor(Math.random() * 3);
+    return {
+        move: move,
+        player: "Computer"
+    };
+}
+function handleUserChoice(choice) {
+    clearMessage();
+    var userGuess = {
+        move: choice,
+        player: "User"
+    };
+    var computerGuess = getComputerMove();
+    outputMessage("You chose " + moves[userGuess.move]);
+    outputMessage("Computer chose " + moves[computerGuess.move]);
+    var winner = calculateWinner(userGuess, computerGuess);
+    if (winner.player === "User")
+        userScore++;
+    if (winner.player === "Computer")
+        computerScore++;
+    outputMessage(winner.player + " wins with " + moves[winner.move]);
+}
+function calculateWinner(guessOne, guessTwo) {
+    if (guessOne.move == guessTwo.move) {
+        return { player: "Neither", move: guessOne.move };
+    }
+    switch (guessOne.move) {
+        case moves.Rock:
+            if (guessTwo.move === moves.Paper)
+                return guessTwo;
+            break;
+        case moves.Paper:
+            if (guessTwo.move === moves.Scissors)
+                return guessTwo;
+            break;
+        case moves.Scissors:
+            if (guessTwo.move === moves.Rock)
+                return guessTwo;
+            break;
+        default:
+            return guessOne;
+    }
+    return guessOne;
 }
 //# sourceMappingURL=game.js.map
--- ts/game.ts
+++ ts/game.ts
@@ -1,3 +1,9 @@
+enum moves {
+    Rock,
+    Paper,
+    Scissors
+}
+
 let userScore: number = 0;
 let computerScore: number = 0;
 let counter: number = 0;
@@ -11,10 +17,67 @@ function startGame(): void {
 }
 
 function outputMessage(message: string): void {
-    alert(message);
+    let output: HTMLDivElement = document.querySelector(".output");
+    output.innerHTML += `<p>${message}</p>`;
+}
+
+function clearMessage(): void {
+    let output: HTMLDivElement = document.querySelector(".output");
+    output.innerHTML = "";
 }
 
 function hideStartButton(): void {
     let startButton: HTMLButtonElement = document.querySelector(".start-button");
+    let gameButtons: HTMLDivElement = document.querySelector(".game-buttons");
     startButton.style.display = "none";
+    gameButtons.style.display = "block";
+}
+
+interface guess {
+    move: number, // 0, 1 or 2
+    player: string // "User" or "Computer"
+}
+  
+function getComputerMove(): guess {
+    let move = Math.floor(Math.random() * 3);
+    return {
+        move: move,
+        player: "Computer"
+    }
+}
+
+function handleUserChoice(choice: number): void {
+    clearMessage();
+    let userGuess: guess = {
+        move: choice,
+        player: "User"
+    }
+    let computerGuess: guess = getComputerMove();
+    outputMessage(`You chose ${moves[userGuess.move]}`);
+    outputMessage(`Computer chose ${moves[computerGuess.move]}`);
+    let winner: guess = calculateWinner(userGuess, computerGuess);
+    if (winner.player === "User") userScore++;
+    if (winner.player === "Computer") computerScore++;
+    outputMessage(`${winner.player} wins with ${moves[winner.move]}`);
+}
+
+function calculateWinner(guessOne: guess, guessTwo: guess): guess {
+    if (guessOne.move == guessTwo.move) {
+        return { player: "Neither", move: guessOne.move };
+    }
+  
+    switch (guessOne.move) {
+        case moves.Rock:
+            if (guessTwo.move === moves.Paper) return guessTwo
+            break
+        case moves.Paper:
+            if (guessTwo.move === moves.Scissors) return guessTwo
+            break
+        case moves.Scissors:
+            if (guessTwo.move === moves.Rock) return guessTwo
+            break
+        default:
+            return guessOne
+    }
+    return guessOne
 }
